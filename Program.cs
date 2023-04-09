d73aec43024e6fece309c3c4a500654f58ee27f0
Manuel Chavez <manuel1995_01@outlook.com>
2023-04-08 20:29:37 -0500

    init

--- Program.cs
+++ Program.cs
@@ -1,30 +1,72 @@
-﻿var steps = 12;
-var path = "DDUUDDUDUUUD";
-var pathChar = path.ToCharArray().ToList();
-var valleyStart = 0;
-var valleyCount = 0;
+﻿var clsnote = new NotesStore();
 
-for (int i = 0; i < steps; i++)
+clsnote.AddNote("active", "DrinkTea");
+clsnote.AddNote("active", "DrinkCoffee");
+clsnote.AddNote("completed", "DrinkCoffee");
+
+var result = clsnote.GetNotes("active");
+
+Console.WriteLine(result);
+public class NotesStore
 {
-    if (path[i] == 'U')
+    public static Dictionary<string, List<string>> Result { get; set; }  = new Dictionary<string, List<string>>();
+    public List<string> statesResult = new List<string>();
+    public void AddNote(string state, string name)
     {
-        valleyStart +=1;
-    }
-    else
-    {
-        valleyStart -=1;
+        try
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new Exception("Name cannot be empty");
+            
+            var stateFormated = state.ToLower();
+
+            if (ValidateState(stateFormated))
+            {
+                statesResult.Add(name);
+                Result.Add(stateFormated, statesResult);
+            }
+        }
+        catch (System.Exception ex)
+        {
+            Result.Add("error", new List<string>{ex.Message});
+        }
     }
 
-    if (valleyStart == 0 && path[i] == 'U')
-        valleyCount+=1;
+    private bool ValidateState(string state)
+    {
+        switch (state)
+        {
+            case "completed":
+            case "active":
+            case "others":
+                return true;
+            default:
+                throw new Exception($"Error: Invalid state {state}");
+        }
+    }
+
+    public List<String> GetNotes(string state)
+    {
+        var retrn = new List<string>();
+        var st = state.ToLower();
+        switch (st)
+        {
+            case "completed":
+                retrn.AddRange(Result[st]);
+                break;
+            case "active":
+                retrn.AddRange(Result[st]);
+                break;
+            case "others":
+                retrn.AddRange(Result[st]);
+                break;
+            default:
+                retrn.Add($"Error: Invalid state {state}");
+                break;
+                //throw new Exception($"Invalid state {state}");
+        }
+
+        return retrn;
+    }
 }
 
-Console.WriteLine(valleyCount);
-
-// _/\      _
-//    \    /
-//     \/\/
-
-// _          /\ 
-//  \  /\    /
-//   \/  \/\/
