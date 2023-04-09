9f996fa8e940ccd6746c167fdb5b24e6e82c1729
Manuel Chavez <manuel1995_01@outlook.com>
2023-04-08 20:28:58 -0500

    counting valleys

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var steps = 12;
+var path = "DDUUDDUDUUUD";
+var pathChar = path.ToCharArray().ToList();
+var valleyStart = 0;
+var valleyCount = 0;
+
+for (int i = 0; i < steps; i++)
+{
+    if (path[i] == 'U')
+    {
+        valleyStart +=1;
+    }
+    else
+    {
+        valleyStart -=1;
+    }
+
+    if (valleyStart == 0 && path[i] == 'U')
+        valleyCount+=1;
+}
+
+Console.WriteLine(valleyCount);
+
+// _/\      _
+//    \    /
+//     \/\/
+
+// _          /\ 
+//  \  /\    /
+//   \/  \/\/
