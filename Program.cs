b45a476e58b90cff29b012c106156d8918d62104
Manuel Chavez <manuel1995_01@outlook.com>
2023-04-08 20:35:28 -0500

    Plus minus

--- Program.cs
+++ Program.cs
@@ -1,17 +1,16 @@
-﻿var ar = new List<int> {10, 20, 20, 10, 10, 30, 50, 10, 20 }; 
-int pairCount = 0;
+﻿var arr = new List<int> {-4, 3, -9, 0, 4, 1}; 
 
-Dictionary<int, int> dict = new Dictionary<int, int>();
-for (int i = 0; i < ar.Count; i++) {
-    if (dict.ContainsKey(ar[i])) {
-        dict[ar[i]]++;
-    } else {
-        dict[ar[i]] = 1;
-    }
-}
+List<int> positives = new List<int>();
+List<int> negatives = new List<int>();
+List<int> zeros = new List<int>();
 
-foreach (int value in dict.Values) {
-    pairCount += value / 2;
-}
+foreach(var num in arr)
+{
+    if(num > 0) positives.Add(num);
+    if(num == 0) zeros.Add(num);
+    if(num < 0) negatives.Add(num);
+}       
 
-return pairCount;
+Console.WriteLine(Math.Round((double)positives.Count() / arr.Count(), 6));
+Console.WriteLine(Math.Round((double)negatives.Count() / arr.Count(), 6));
+Console.WriteLine(Math.Round((double)zeros.Count() / arr.Count(), 6));
