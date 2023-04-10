399db0119240c17fc5b424cca007506e2ca43541
Manuel Chavez <manuel1995_01@outlook.com>
2023-04-10 18:10:37 -0500

    flipping matrix - Medium

--- Program.cs
+++ Program.cs
@@ -1,72 +1,38 @@
-﻿var clsnote = new NotesStore();
-
-clsnote.AddNote("active", "DrinkTea");
-clsnote.AddNote("active", "DrinkCoffee");
-clsnote.AddNote("completed", "DrinkCoffee");
-
-var result = clsnote.GetNotes("active");
-
-Console.WriteLine(result);
-public class NotesStore
+﻿var matrix = new List<List<int>>
 {
-    public static Dictionary<string, List<string>> Result { get; set; }  = new Dictionary<string, List<string>>();
-    public List<string> statesResult = new List<string>();
-    public void AddNote(string state, string name)
-    {
-        try
-        {
-            if (string.IsNullOrWhiteSpace(name))
-                throw new Exception("Name cannot be empty");
-            
-            var stateFormated = state.ToLower();
+    // new List<int> {112,42,83,119},
+    // new List<int> {56,125,56,49},
+    // new List<int> {15,78,108,43},
+    // new List<int> {62,98,114,108},
+    new List<int> {1,2},
+    new List<int> {3,4}
+};
 
-            if (ValidateState(stateFormated))
-            {
-                statesResult.Add(name);
-                Result.Add(stateFormated, statesResult);
-            }
-        }
-        catch (System.Exception ex)
-        {
-            Result.Add("error", new List<string>{ex.Message});
-        }
-    }
+Console.WriteLine(flippingMatrix(matrix));
 
-    private bool ValidateState(string state)
-    {
-        switch (state)
-        {
-            case "completed":
-            case "active":
-            case "others":
-                return true;
-            default:
-                throw new Exception($"Error: Invalid state {state}");
-        }
-    }
-
-    public List<String> GetNotes(string state)
-    {
-        var retrn = new List<string>();
-        var st = state.ToLower();
-        switch (st)
-        {
-            case "completed":
-                retrn.AddRange(Result[st]);
-                break;
-            case "active":
-                retrn.AddRange(Result[st]);
-                break;
-            case "others":
-                retrn.AddRange(Result[st]);
-                break;
-            default:
-                retrn.Add($"Error: Invalid state {state}");
-                break;
-                //throw new Exception($"Invalid state {state}");
-        }
-
-        return retrn;
-    }
+static int flippingMatrix(List<List<int>> matrix)
+{
+   int queryLength = matrix[0].Count() - 1;
+   int halfQueryLength = (matrix[0].Count() / 2) - 1;
+   int maxSum = 0;
+   for(int row = 0; row <= halfQueryLength; row++){
+      for(int col = 0; col <= halfQueryLength; col++){
+         var counter = new List<int>(){
+            matrix[row][col],
+            matrix[row][queryLength - col],
+            matrix[queryLength - row][col],
+            matrix[queryLength - row][queryLength - col],
+         };
+         maxSum += biggerNumber(counter);
+      }
+   }
+   return maxSum;
 }
 
+static int biggerNumber(List<int> numberList){
+   int bigger = 0;
+   foreach(var num in numberList){
+      bigger = Math.Max(bigger, num);
+   }
+   return bigger;
+}
