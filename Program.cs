bc17b82fea5670402312a0a93ca08446c70e077d
Manuel Chavez <manuel1995_01@outlook.com>
2023-04-09 20:34:51 -0500

    Knowing whether its pangram or not.

--- Program.cs
+++ Program.cs
@@ -1,16 +1,13 @@
-﻿var n = 4;
-var stairs = new List<string>();
+﻿var s = "the quick brown fox jumps over the lazy dog";
+s = s.ToLower();
+char[] alphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
 
-for (int i = 0; i < n; i++)
+foreach (var letter in alphabet)
 {
-    var chr = string.Empty;
-    var padded = string.Empty;
-    for (int j = 0; j <= i; j++)
+    if (!s.Contains(letter))
     {
-        chr += "#";
+        Console.WriteLine("not pangram");
     }
-    padded = chr.PadLeft(n);
-    stairs.Add(padded);
 }
-    
-stairs.ForEach(s => Console.WriteLine(s));
+
+Console.WriteLine("pangram");
