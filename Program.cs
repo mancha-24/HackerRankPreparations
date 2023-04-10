fdc1ef60fd255e563533e318db577ff7235cf843
Manuel Chavez <manuel1995_01@outlook.com>
2023-04-10 13:01:12 -0500

    Mars exploration SOS signals

--- Program.cs
+++ Program.cs
@@ -1,13 +1,22 @@
-﻿var s = "the quick brown fox jumps over the lazy dog";
-s = s.ToLower();
-char[] alphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+﻿       //1  23 456 78901   234   56  78   90
+       //SOSSOSSOSSOSSOSSOSSOSSOSSOSSOSSOSSOS
+var s = "OOSDSSOSOSWEWSOSOSOSOSOSOSSSSOSOSOSS";
+var chr = s.ToCharArray();
+var countSignals = 0;
+var countErrors = 0;
+var sos = "SOS".ToCharArray();
 
-foreach (var letter in alphabet)
+if (chr.Count()%3 == 0)
+    countSignals = chr.Count() / 3;
+
+for (int i = 0; i < countSignals; i++)
 {
-    if (!s.Contains(letter))
+    var signal = s.Substring(i*3,3);
+    for (int j = 0; j < sos.Count(); j++)
     {
-        Console.WriteLine("not pangram");
+        if (signal[j] != sos[j])
+            countErrors++;
     }
 }
 
-Console.WriteLine("pangram");
+Console.WriteLine(countErrors);
