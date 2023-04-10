9caf573f616f3d71373d8be60b203f787334aed0
Manuel Chavez <manuel1995_01@outlook.com>
2023-04-10 18:15:22 -0500

    array reverse

--- Program.cs
+++ Program.cs
@@ -1,22 +1,6 @@
-﻿       //1  23 456 78901   234   56  78   90
-       //SOSSOSSOSSOSSOSSOSSOSSOSSOSSOSSOSSOS
-var s = "OOSDSSOSOSWEWSOSOSOSOSOSOSSSSOSOSOSS";
-var chr = s.ToCharArray();
-var countSignals = 0;
-var countErrors = 0;
-var sos = "SOS".ToCharArray();
-
-if (chr.Count()%3 == 0)
-    countSignals = chr.Count() / 3;
-
-for (int i = 0; i < countSignals; i++)
+﻿var a = new List<int> 
 {
-    var signal = s.Substring(i*3,3);
-    for (int j = 0; j < sos.Count(); j++)
-    {
-        if (signal[j] != sos[j])
-            countErrors++;
-    }
-}
+    1,4,3,2
+};
 
-Console.WriteLine(countErrors);
+a.Reverse();
