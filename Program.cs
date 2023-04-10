9f79cd09061bbab37f9b53527ce5502bb13d3176
Manuel Chavez <manuel1995_01@outlook.com>
2023-04-09 20:24:11 -0500

    Stair case

--- Program.cs
+++ Program.cs
@@ -1,16 +1,16 @@
-﻿var arr = new List<int> {-4, 3, -9, 0, 4, 1}; 
+﻿var n = 4;
+var stairs = new List<string>();
 
-List<int> positives = new List<int>();
-List<int> negatives = new List<int>();
-List<int> zeros = new List<int>();
-
-foreach(var num in arr)
+for (int i = 0; i < n; i++)
 {
-    if(num > 0) positives.Add(num);
-    if(num == 0) zeros.Add(num);
-    if(num < 0) negatives.Add(num);
-}       
-
-Console.WriteLine(Math.Round((double)positives.Count() / arr.Count(), 6));
-Console.WriteLine(Math.Round((double)negatives.Count() / arr.Count(), 6));
-Console.WriteLine(Math.Round((double)zeros.Count() / arr.Count(), 6));
+    var chr = string.Empty;
+    var padded = string.Empty;
+    for (int j = 0; j <= i; j++)
+    {
+        chr += "#";
+    }
+    padded = chr.PadLeft(n);
+    stairs.Add(padded);
+}
+    
+stairs.ForEach(s => Console.WriteLine(s));
