98ba878cfbe9ebd68899d5cc9847129f00a01bbc
Manuel Chavez <manuel1995_01@outlook.com>
2023-04-11 15:57:53 -0500

    Permuting 2 arrays

--- Program.cs
+++ Program.cs
@@ -1,27 +1,13 @@
-﻿List<List<int>> arr = new List<List<int>>
-{
-    new List<int> {1, 1, 1, 0, 0, 0},
-    new List<int> {0, 1, 0, 0, 0, 0},
-    new List<int> {1, 1, 1, 0, 0, 0},
-    new List<int> {0, 0, 2, 4, 4, 0},
-    new List<int> {0, 0, 0, 2, 0, 0},
-    new List<int> {0, 0, 1, 2, 4, 0}
-};
+﻿var A = new List<int> {1, 2, 2, 1};
+var B = new List<int> {3, 3, 3, 4};
+var k = 5;
 
-var lengthX = arr[0].Count; //row
-var lengthY = arr.Count; //col
-var sumTotal = new List<int>();
-
-for (int x = 0; x < 4; x++)
+A.Sort();
+B.Sort();
+B.Reverse();
+for (int i = 0; i < A.Count; i++)
 {
-    var sumHourGlass = 0;
-    for (int y = 0; y < 4; y++)
-    {
-        sumHourGlass = arr[x][y] + arr[x][y+1] + arr[x][y+2] +
-                                   arr[x+1][y+1] +
-                       arr[x+2][y] + arr[x+2][y+1] + arr[x+2][y+2];
-        sumTotal.Add(sumHourGlass);
-    }
+    if (A[i] + B[i] < k)
+        Console.WriteLine("NO");
 }
-
-Console.WriteLine(sumTotal.Max());
+Console.WriteLine("YES");
