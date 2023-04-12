d9780e37ef2a9ef4f28ba6f0addf0cb3de1e9f1b
Manuel Chavez <manuel1995_01@outlook.com>
2023-04-12 10:06:36 -0500

    Birthday Cake Candles

--- Program.cs
+++ Program.cs
@@ -1,17 +1,5 @@
-﻿var ar = new List<int> {10, 20, 20, 10, 10, 30, 50, 10, 20 }; 
-int pairCount = 0;
+﻿var candles = new List<int> {3,2,1,3};
+var max = candles.Max();
+var countMax = candles.Count(c => c == max);
 
-Dictionary<int, int> dict = new Dictionary<int, int>();
-for (int i = 0; i < ar.Count; i++) {
-    if (dict.ContainsKey(ar[i])) {
-        dict[ar[i]]++;
-    } else {
-        dict[ar[i]] = 1;
-    }
-}
-
-foreach (int value in dict.Values) {
-    pairCount += value / 2;
-}
-
-return pairCount;
+Console.WriteLine(countMax);
