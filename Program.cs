aef4830d31800de7369871b5a7c10d2a2db2e920
Manuel Chavez <manuel1995_01@outlook.com>
2023-04-18 18:06:18 -0500

    kangaroos Number lines jump

--- Program.cs
+++ Program.cs
@@ -1,25 +1,7 @@
-﻿// s: integer, starting point of Sam's house location.
-// t: integer, ending location of Sam's house location.
-// a: integer, location of the Apple tree.
-// b: integer, location of the Orange tree.
-// apples: integer array, distances at which each apple falls from the tree.
-// oranges: integer array, distances at which each orange falls from the tree.
+﻿int x1 = 0; //posicion canguro 1
+int v1 = 3; //pasos por salto canguro 1
+int x2 = 4; //posicion canguro 2
+int v2 = 2; //pasos por salto canguro 2
 
-var s = 7;
-var t = 11;
-var a = 5;
-var b = 15;
-var apples = new List<int>
-{
-    -2,2,1
-};
-var oranges = new List<int>
-{
-    5,-6
-};
-
-var countApples = apples.Count(ap => (ap+a) >= s && (ap+a) <= t);
-var countOranges = oranges.Count(or => (or+b) >= s && (or+b) <= t);
-
-Console.WriteLine(countApples);
-Console.WriteLine(countOranges);
+if (v1 > v2 && (x2 - x1)%(v1 - v2) == 0) Console.WriteLine("YES");
+Console.WriteLine("NO");
